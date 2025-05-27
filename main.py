9c5e5f3846c916e26fb9eb3634f851f6ca29fce8
Noel1812 <noelhildenbrand@web.de>
2025-05-27 15:03:25 +0200

    Add files via upload
    Main

---
+++ main.py
@@ -0,0 +1,35 @@
+from config.loader import load_config
+from network.messaging import send_message, start_listening
+import threading
+
+# Datei aus settings.toml einlesen
+import sys
+
+# Optional: Dateiname per Argument übergeben
+config_path = sys.argv[1] if len(sys.argv) > 1 else "config/settings.toml"
+config = load_config(config_path)
+
+handle = config["handle"]
+my_port = config["port"]
+peers = config["peers"]
+
+#  Konfiguration ausgeben 
+print("Handle:", handle)
+print("Port:", my_port)
+print("Peers:", peers)
+print("Starte Listener...")
+
+# listener
+threading.Thread(target=start_listening, args=(my_port,), daemon=True).start()
+
+# while schleife um Nachrichten einzugeben 
+print("Gib eine Nachricht ein (oder 'exit' zum Beenden):")
+while True:
+    message = input("> ")
+    if message == "exit":
+        break
+
+    full_message = f"{handle}: {message}"
+    for peer in peers:
+        ip, port = peer.split(":")
+        send_message(full_message, ip, int(port))
